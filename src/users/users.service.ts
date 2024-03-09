d54a0e51be3fb9260464746543fa22589304784f
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-03-09 14:25:38 +0100

    add simple User resource

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from '../database/database.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({ data: createUserDto });
+  }
+
+  async findAll() {
+    return this.databaseService.user.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.user.findUnique({ where: { id } });
+  }
+
+  async update(id: number, updateUserDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      data: updateUserDto,
+      where: { id: id },
+    });
+  }
+
+  async remove(id: number) {
+    return this.databaseService.user.delete({ where: { id } });
+  }
+}
