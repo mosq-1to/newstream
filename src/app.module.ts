2897e93972a7df98ef0bc522056977b70e73556b
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-06-16 14:19:15 +0200

    create modules dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule],
