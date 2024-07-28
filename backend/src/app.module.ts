af36b096a7c7f0e0abd6002c9216782e4786ec27
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-07-28 17:54:47 +0200

    add auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { TextToSpeechModule } from './generative/text-to-speech/text-to-speech.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TextToSpeechModule } from './generative/text-to-speech/text-to-speech.m
     DatabaseModule,
     UsersModule,
     TextToSpeechModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
