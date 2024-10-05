b593bc94362e7da18bff1bfa7ef8b42fc6f5c201
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-10-05 12:54:00 +0200

    add AUTO_ARTICLES_FETCHER_ENABLED feature flag

--- backend/src/modules/articles/articles.tasks.ts
+++ backend/src/modules/articles/articles.tasks.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { EVERYDAY_AT_10_AND_20 } from '../../utils/time/cron-expressions';
 import { ArticlesService } from './articles.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ArticlesTasks {
-  constructor(private readonly articlesService: ArticlesService) {}
+  constructor(
+    private readonly articlesService: ArticlesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Cron(EVERYDAY_AT_10_AND_20)
   async fetchLatestArticlesToDatabase() {
+    if (this.configService.get('AUTO_ARTICLES_FETCHER_ENABLED') !== 'true') {
+      return;
+    }
+
     const articles = await this.articlesService.getLatestArticles();
     await this.articlesService.saveArticlesToDatabase(articles);
   }
