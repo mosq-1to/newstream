a6250bf5615217017ba97d38f5ea6a5fefcf022e
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-10-15 18:31:54 +0200

    add thumbnailUrl to Story dto

--- client_app/lib/api/newstream/stories/story_model.dart
+++ client_app/lib/api/newstream/stories/story_model.dart
@@ -3,12 +3,14 @@ class Story {
   final String title;
   final String url;
   final String content;
+  final String thumbnailUrl;
 
   const Story({
     required this.id,
     required this.title,
     required this.url,
     required this.content,
+    required this.thumbnailUrl,
   });
 
   factory Story.fromJson(Map<String, dynamic> json) {
@@ -18,12 +20,14 @@ class Story {
         'title': final String title,
         'url': final String url,
         'content': final String content,
+        'thumbnailUrl': final String thumbnailUrl,
       } =>
         Story(
           id: id,
           title: title,
           url: url,
           content: content,
+          thumbnailUrl: thumbnailUrl,
         ),
       _ => throw const FormatException('Failed to load Story.'),
     };
