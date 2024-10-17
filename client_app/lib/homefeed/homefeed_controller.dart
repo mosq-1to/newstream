b72afcff730ce3a9b12d05f4477cb8a3dfa6338c
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-10-17 08:25:18 +0200

    remove redundant query from HomefeedController

--- client_app/lib/homefeed/homefeed_controller.dart
+++ client_app/lib/homefeed/homefeed_controller.dart
@@ -1,22 +1,19 @@
-import 'package:client_app/api/newstream/auth/current_user_model.dart';
 import 'package:client_app/api/newstream/newstream_api.dart';
 import 'package:client_app/api/newstream/stories/story_model.dart';
 import 'package:get/get.dart';
 
 class HomefeedController extends GetxController {
-  final Rx<CurrentUser?> currentUser = Rx<CurrentUser?>(null);
   final NewstreamApi _newstreamApi = Get.find();
   final RxList<Story> stories = RxList<Story>([]);
 
   @override
   Future<void> onInit() async {
     super.onInit();
-    currentUser.value = await _newstreamApi.getCurrentUser();
     stories.value = await _newstreamApi.getStories();
   }
 
   void openStory(Story story) {
-    // Open story
+    // TODO Open story
     print('Story opened: ${story.id}');
   }
 }
