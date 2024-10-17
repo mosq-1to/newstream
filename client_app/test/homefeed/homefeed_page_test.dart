4d8497b7169c108cb2b3414f035b1b5ee98885d8
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2024-10-17 09:17:22 +0200

    add basic tests for homefeed page

--- client_app/test/homefeed/homefeed_page_test.dart
+++ client_app/test/homefeed/homefeed_page_test.dart
@@ -1,5 +1,58 @@
+import 'package:client_app/api/newstream/newstream_api.dart';
+import 'package:client_app/api/newstream/stories/story_model.dart';
+import 'package:client_app/homefeed/homefeed_controller.dart';
+import 'package:client_app/homefeed/homefeed_page.dart';
+import 'package:client_app/homefeed/widgets/stories_list_entry.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+
+import '../api/newstream_api_test.mocks.dart';
 
 void main() {
-  group('renders stories', () {});
+  late MockNewstreamApi mockNewstreamApi;
+  late HomefeedController homefeedController;
+
+  setUp(() {
+    mockNewstreamApi = MockNewstreamApi();
+    Get.put<NewstreamApi>(mockNewstreamApi);
+
+    homefeedController = HomefeedController();
+    Get.lazyPut<HomefeedController>(() => homefeedController);
+  });
+
+  tearDown(() {
+    Get.delete<NewstreamApi>();
+    Get.delete<HomefeedController>();
+  });
+
+  testWidgets('renders stories', (WidgetTester tester) async {
+    // Given
+    when(mockNewstreamApi.getStories()).thenAnswer(
+      (_) async => Future.value([
+        const Story(
+          id: '1',
+          title: 'Story 1',
+          content: 'Content 1',
+          url: '',
+          thumbnailUrl: '',
+        ),
+        const Story(
+          id: '2',
+          title: 'Story 2',
+          content: 'Content 2',
+          url: '',
+          thumbnailUrl: '',
+        ),
+      ]),
+    );
+
+    // When
+    await tester.pumpWidget(GetMaterialApp(home: HomefeedPage()));
+    await tester.pump();
+
+    // Then
+    expect(find.text('Recent stories'), findsOneWidget);
+    expect(tester.widgetList(find.byType(StoriesListEntry)).length, 2);
+  });
 }
