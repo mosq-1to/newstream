db8c7b8cf1597f2765f26427f0ab57f473fb22e4
Mateusz Komarnicki <mateusz.komarnicki@ramp.network>
2025-03-24 08:32:23 +0100

    remove another dead code

--- backend/src/modules/audio-generation/hls/hls.service.ts
+++ backend/src/modules/audio-generation/hls/hls.service.ts
@@ -17,53 +17,6 @@ export class HlsService {
     void this.ensureDirectoryExists(this.HLS_OUTPUT_DIR);
   }
 
-  /**
-   * Converts an audio file to HLS format
-   * @param inputFilePath Path to the input audio file
-   * @param streamId Custom ID for the HLS stream
-   * @returns ID of the HLS stream and path to the playlist file
-   */
-  async convertToHls(
-    inputFilePath: string,
-    streamId: string,
-  ): Promise<{ streamId: string; playlistPath: string }> {
-    // Create a directory for this stream
-    const streamDir = path.join(this.HLS_OUTPUT_DIR, streamId, 'stream');
-    await this.ensureDirectoryExists(streamDir);
-    const streamSegmentsDir = path.join(streamDir, 'segments');
-    await this.ensureDirectoryExists(streamSegmentsDir);
-    // Output playlist file
-    const playlistFile = path.join(streamDir, 'playlist.m3u8');
-
-    return new Promise((resolve, reject) => {
-      ffmpeg(inputFilePath)
-        .outputOptions([
-          '-f hls',
-          '-hls_time 4',
-          '-hls_playlist_type vod',
-          '-hls_segment_filename',
-          path.join(streamSegmentsDir, 'segment_%03d.ts'),
-          '-hls_list_size 0',
-          '-hls_base_url',
-          'stream/segments/',
-          '-acodec aac',
-          '-ar 44100',
-          '-ab 128k',
-        ])
-        .output(playlistFile)
-        .on('end', () => {
-          resolve({
-            streamId,
-            playlistPath: playlistFile,
-          });
-        })
-        .on('error', (err) => {
-          reject(err);
-        })
-        .run();
-    });
-  }
-
   private async initializeEmptyPlaylist(streamId: string): Promise<string> {
     const streamDir = path.join(this.HLS_OUTPUT_DIR, streamId, 'stream');
     await this.ensureDirectoryExists(streamDir);
