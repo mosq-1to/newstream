2c116ffdd12d5f7d6e25114bd42ec9582134e7df
Komarnicki Mateusz <komarnicki115@gmail.com>
2025-05-06 12:54:46 +0200

    refactor the player logic

--- client_app/lib/player/player_controller.dart
+++ client_app/lib/player/player_controller.dart
@@ -6,31 +6,20 @@ import 'package:client_app/player/player_model.dart';
 import 'package:get/get.dart';
 import 'package:just_audio/just_audio.dart' as audio;
 
+/// TODO
+/// - add processing state which will be used after clicking and before playing has started
+
 class PlayerController extends GetxController {
   final NewstreamApi _newstreamApi = Get.find();
   final Rx<PlayerState> playerState = const PlayerState().obs;
   final audio.AudioPlayer _audioPlayer = audio.AudioPlayer();
 
   Future<void> togglePlayPause() async {
-    try {
-      if (playerState.value.isPlaying) {
-        await _audioPlayer.pause();
-      } else {
-        await _audioPlayer.play();
-      }
-
-      playerState.value = playerState.value.copyWith(
-        isPlaying: !playerState.value.isPlaying,
-      );
-    } catch (e) {
-      developer.log('Error toggling play/pause: $e');
+    if (_audioPlayer.playing) {
+      await _audioPlayer.pause();
+    } else {
+      await _audioPlayer.play();
     }
-  }
-
-  void updateProgress(double progress) {
-    playerState.value = playerState.value.copyWith(
-      progress: progress,
-    );
   }
 
   @override
@@ -41,15 +30,28 @@ class PlayerController extends GetxController {
     _audioPlayer.playbackEventStream.listen(
       (event) {},
       onError: (Object e, StackTrace st) {
-        developer.log('Audio player error: $e');
-        developer.log('Stack trace: $st');
-
-        // Update player state on error
-        playerState.value = playerState.value.copyWith(
-          isPlaying: false,
-        );
+        print('Audio player error: $e');
+        print('Stack trace: $st');
       },
     );
+
+    // Listen to player state changes
+    _audioPlayer.playerStateStream.listen((playerState) {
+      this.playerState.value = this.playerState.value.copyWith(
+            isPlaying: playerState.playing,
+          );
+    });
+
+    // Listen to position changes
+    _audioPlayer.positionStream.listen((position) {
+      final duration = _audioPlayer.duration;
+      if (duration != null && duration.inMilliseconds > 0) {
+        final progress = position.inMilliseconds / duration.inMilliseconds;
+        playerState.value = playerState.value.copyWith(
+          progress: progress,
+        );
+      }
+    });
   }
 
   @override
@@ -65,56 +67,16 @@ class PlayerController extends GetxController {
 
       // Update player state to show loading
       playerState.value = PlayerState(
-        isPlaying: false,
-        progress: 0.0,
         currentStory: story,
-        playlistUrl: null,
       );
 
-        try {
-          final playlistUrl = await _newstreamApi.getStoryStreamPlaylistUrl(
-              story.id);
-          developer.log('Trying HLS with setUrl: $playlistUrl');
+      final playlistUrl =
+          await _newstreamApi.getStoryStreamPlaylistUrl(story.id);
 
-          await _audioPlayer.setUrl(playlistUrl);
-          await _audioPlayer.play();
-
-          playerState.value = playerState.value.copyWith(
-            playlistUrl: playlistUrl,
-          );
-
-          developer.log('Successfully playing with HLS setUrl');
-        } catch (e) {
-          developer.log('Error playing with HLS setUrl: $e');
-        }
-
-
-      // Listen to player state changes
-      _audioPlayer.playerStateStream.listen((playerState) {
-        developer.log('Player state changed: ${playerState.processingState}');
-
-        // Update isPlaying based on player state
-        this.playerState.value = this.playerState.value.copyWith(
-          isPlaying: playerState.playing,
-        );
-      });
-
-      // Listen to position changes
-      _audioPlayer.positionStream.listen((position) {
-        final duration = _audioPlayer.duration;
-        if (duration != null && duration.inMilliseconds > 0) {
-          final progress = position.inMilliseconds / duration.inMilliseconds;
-          updateProgress(progress);
-        }
-      });
-
-      // Update player state
-      playerState.value = playerState.value.copyWith(
-        isPlaying: true,
-      );
-    } catch (e, st) {
-      developer.log('Error playing story: $e');
-      developer.log('Stack trace: $st');
+      await _audioPlayer.setUrl(playlistUrl);
+      await _audioPlayer.play();
+    } catch (e) {
+      print('Error playing story: $e');
 
       // Handle error gracefully
       playerState.value = playerState.value.copyWith(
