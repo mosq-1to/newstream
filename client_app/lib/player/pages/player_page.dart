2d97842387c4a37a049b3f07faec200766a71e72
Komarnicki Mateusz <komarnicki115@gmail.com>
2025-05-15 13:50:38 +0200

    add source articles underneath the player

--- client_app/lib/player/pages/player_page.dart
+++ client_app/lib/player/pages/player_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:client_app/api/newstream/models/article_model.dart';
 import 'package:client_app/api/newstream/models/brief_model.dart';
 import 'package:client_app/common/theme/text_styles.dart';
 import 'package:client_app/player/player_controller.dart';
@@ -7,6 +8,8 @@ import 'package:client_app/player/widgets/player_controls.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:client_app/common/ui/tappable.dart';
 
 class PlayerPage extends StatefulWidget {
   const PlayerPage({super.key});
@@ -124,6 +127,8 @@ class _PlayerPageState extends State<PlayerPage> {
                                         ),
                                         const SizedBox(height: 48),
                                         PlayerControls(),
+                                        const SizedBox(height: 48),
+                                        _buildSourcesSection(currentBrief.articles),
                                       ],
                                     ),
                                   ),
@@ -162,6 +167,95 @@ class _PlayerPageState extends State<PlayerPage> {
         //   maxLines: 2,
         //   overflow: TextOverflow.ellipsis,
         // ),
+      ],
+    );
+  }
+
+  Widget _buildSourcesSection(List<Article> articles) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Sources',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 16),
+        ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: articles.length,
+          separatorBuilder: (context, index) => const SizedBox(height: 12),
+          itemBuilder: (context, index) {
+            final article = articles[index];
+            return Tappable(
+              onTap: () async {
+                final url = Uri.parse(article.url);
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url, mode: LaunchMode.externalApplication);
+                }
+              },
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Article thumbnail
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: article.thumbnailUrl.isNotEmpty
+                        ? Image.network(
+                            article.thumbnailUrl,
+                            width: 80,
+                            height: 60,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                width: 80,
+                                height: 60,
+                                color: Colors.grey[800],
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.image_not_supported,
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                              );
+                            },
+                          )
+                        : Container(
+                            width: 80,
+                            height: 60,
+                            color: Colors.grey[800],
+                            child: const Center(
+                              child: Icon(
+                                Icons.article,
+                                color: Colors.white54,
+                              ),
+                            ),
+                          ),
+                  ),
+                  const SizedBox(width: 12),
+                  // Article title
+                  Expanded(
+                    child: Text(
+                      article.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 24),
       ],
     );
   }
